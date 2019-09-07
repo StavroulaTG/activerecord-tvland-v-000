64b2c7006d027548b375cfa05396cd326056c94a
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-07 12:59:10 +0000

    Automatically backed up by Learn

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,6 +1,7 @@
 class Network < ActiveRecord::Base
   has_many :shows
   has_many :characters, through: :shows
+  has_many :actors
 
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
