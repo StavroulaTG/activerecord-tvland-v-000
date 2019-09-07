0cd1d471e61ba0525ff7ebab35bfe0bf15ae5000
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-07 13:24:19 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,8 +2,8 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :show_id
-      t.string :actor_id
+      t.integer :show_id
+      t.integer :actor_id
     end
   end
 end
